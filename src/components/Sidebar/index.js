cf17aa1567f351635e9c02276e76492fa7bc040a
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-04-04 11:26:19 -0500

    starting sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import {Link} from 'react-router-dom'
+import joshface from '../..assets/images/'
+
+const Sidebar = () => {
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            
+        </Link>
+    </div>
+}
+export default Sidebar
