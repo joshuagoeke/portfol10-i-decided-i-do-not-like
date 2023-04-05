ca05646004ef3a523ddd636f651c34635866b373
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-04-05 09:42:46 -0500

    installing packages and getting navbar going

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,28 @@
 import './index.scss'
 import {Link} from 'react-router-dom'
-import joshface from '../..assets/images/'
+import joshface from '../../assets/images/JoshFace.jpg'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
+    return (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            
+        <Link className='josh-face' to='/'>
+            <img src={joshface} alt='joshface'/>
         </Link>
-    </div>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+    </div> 
+    
+   ) 
 }
 export default Sidebar
