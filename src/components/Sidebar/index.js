1433d34a080549cd8e75b1a3f076ca5166e37908
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-04-06 08:23:02 -0500

    more navbar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,15 @@
 import './index.scss'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import joshface from '../../assets/images/JoshFace.jpg'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
     return (
     <div className='nav-bar'>
-        <Link className='josh-face' to='/'>
-            <img src={joshface} alt='joshface'/>
+        <Link  to='/'>
+            <img className='josh-face'src={joshface} alt='joshface'/>
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
@@ -21,6 +22,18 @@ const Sidebar = () => {
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/joshuagoeke/">
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://github.com/joshuagoeke">
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
+                </a>
+            </li>
+        </ul>
     </div> 
     
    ) 
